0ae9a6a1eb95ff4017f4fcdaf417a17ac065e4a1
Amanda Haynes <dev@portad.ca>
2020-01-21 23:22:33 -0700

    placeholder image

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ const withSass = require("@zeit/next-sass");
 const withCSS = require("@zeit/next-css");
 module.exports = withCSS(
   withSass({
+    cssModules: true,
     webpack(config, options) {
       config.node = {
         fs: "empty"
