a1019001799a688c4031c13d71c909f87a478207
Amanda Haynes <dev@portad.ca>
2020-02-04 21:20:29 -0700

    homepage placeholder and pages set up

---
+++ pages/lineup/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Head from "../../components/head";
+
+import styles from "./index.module.scss";
+
+export default () => {
+  return (
+    <div>
+      <Head title="Lineup | Impact Music Festival" />
+    </div>
+  );
+};
