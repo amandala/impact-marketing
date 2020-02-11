d954fe201c103467aa80fb0fa355ff2203376f8e
Amanda <amanda.haynes@versett.com>
2020-02-11 13:29:42 -0700

    particles on window detect

--- components/Particles/index.js
+++ components/Particles/index.js
@@ -75,7 +75,7 @@ export default () => (
           }
         },
         interactivity: {
-          detect_on: "canvas",
+          detect_on: "window",
           events: {
             onhover: {
               enable: true,
