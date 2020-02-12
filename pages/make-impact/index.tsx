cbbde8231a4bdebc7d61ee98bae2c3fd5ddae085
Amanda Haynes <dev@portad.ca>
2020-02-11 21:11:46 -0700

    use react-typeform-embed package

--- pages/make-impact/index.tsx
+++ pages/make-impact/index.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import Head from "../../components/head";
+import cx from "classnames";
+import { ReactTypeformEmbed } from "react-typeform-embed";
 
 import styles from "./index.module.scss";
 
@@ -7,7 +9,7 @@ export default () => {
   return (
     <div>
       <Head title="Get Involved | Impact Music Festival" />
-      <div class="typeform-widget" data-url="https://dasrecord.typeform.com/to/elGZqC" data-transparency="100" data-hide-headers=true data-hide-footer=true style="width: 100%; height: 100%;"></div> <script> (function() { var qs,js,q,s,d=document, gi=d.getElementById, ce=d.createElement, gt=d.getElementsByTagName, id="typef_orm", b="https://embed.typeform.com/"; if(!gi.call(d,id)) { js=ce.call(d,"script"); js.id=id; js.src=b+"embed.js"; q=gt.call(d,"script")[0]; q.parentNode.insertBefore(js,q) } })() </script>
+      <ReactTypeformEmbed url="https://dasrecord.typeform.com/to/elGZqC" />
     </div>
   );
 };
