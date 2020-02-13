82f8f199e5312d9ed2e55fe72d4d4b2edf4105d2
Amanda Haynes <dev@portad.ca>
2020-02-12 19:02:02 -0700

    about page content complete

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -80,7 +80,63 @@ export default () => {
             </P>
           </Section>
           <Section>
+            <div className={styles.Logos}>
+              <a
+                href="https://partywell.org/"
+                target="_blank"
+                className={styles.LogoWrapper}
+              >
+                <img
+                  className={styles.Logo}
+                  src="pwLogo.png"
+                  alt="PartyWell Logo"
+                />
+              </a>
+              <a
+                href="https://www.dusk2dawnproductions.com/"
+                target="_blank"
+                className={styles.LogoWrapper}
+              >
+                <img
+                  className={styles.Logo}
+                  src="d2dLogo.png"
+                  alt="Dusk2Dawn Logo"
+                />
+              </a>
+            </div>
+          </Section>
+          <Section>
             <h1 className={styles.Impact}>Make Impact</h1>
+            <P>
+              Out there somewhere in the deeps of space, on a world eerily
+              similar to our own, an intelligent species of biped revelled in
+              their prosperity, developing wonderous scientific advances and
+              economic prosperity. But... with the spoils of technology, came a
+              blissful ignorance towards the natural life and overall harmony on
+              the planet.
+            </P>
+            <P>
+              With the increase in toxic emissions slowly consuming the planet
+              this world soon reached a crisis point, causing a dramatic shift
+              in the climate and bringing about widespread devastation. Rising
+              seas soon claimed cities, natural disasters claimed entire
+              continents and their civilization fell apart. THE FEW WHO REMAINED
+              NEARLY LOST ALL HOPE.
+            </P>
+            <P>
+              The bipeds gathered their finest minds and launched a ship out
+              into space with the mission of sharing the STORY of their failed
+              world, preserving their culture and HOPING ANOTHER SPECIES WOULD
+              PAY HEED TO THE LESSON. Their story is a sad one... but.... we
+              have come to your planet because we have hope for ALL OF YOU, THE
+              IMPACT COMMUNITY.
+            </P>
+            <P>
+              Come and join us as we continue to tell this story through music,
+              art and various other means. Participate in our story with
+              ineractive quests and multimedia displays.
+            </P>
+            <P>We Are the IMPACT...</P>
           </Section>
         </div>
       </div>
