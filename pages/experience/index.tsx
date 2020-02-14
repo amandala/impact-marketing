ecafa8ee43e262ff1b91a0a3a570f5eb36c60062
Amanda <amanda.haynes@versett.com>
2020-02-14 14:10:11 -0700

    content in

--- pages/experience/index.tsx
+++ pages/experience/index.tsx
@@ -8,15 +8,38 @@ export default () => {
   return (
     <div>
       <Head title="Experience | Impact Music Festival" />
-      <div className={styles.PageWrapper}>
-        <H1 className={styles.ExperienceHeading}>Experience</H1>
-        <div className={styles.Experience1}>
-          <div className={styles.PhotoContent}>
-            <H2>
-              IMPACT festival is full of surprises. Complete games, challenges
-              and quests for various rewards
-            </H2>
-          </div>
+      <div className={styles.Experience1}>
+        <div className={styles.PhotoContent}>
+          <H1 className={styles.ExperienceHeading}>
+            IMPACT festival is full of surprises!
+          </H1>
+          <H2>
+            Participate in games, challenges, and quests for various rewards.
+          </H2>
+        </div>
+      </div>
+      <div className={styles.Experience2}>
+        <div className={styles.PhotoContent}>
+          <H1 className={styles.ExperienceHeading}>Express yourself!</H1>
+          <H2>
+            We encourage everyone to bring their complete authentic self. Don't
+            hold back ;)
+          </H2>
+        </div>
+      </div>
+      <div className={styles.Experience3}>
+        <div className={styles.PhotoContent}>
+          <H1 className={styles.ExperienceHeading}>Make new connections!</H1>
+          <H2>
+            "Here's to the nights that turned into mornings, and the friends who
+            turned into family"
+          </H2>
+        </div>
+      </div>
+      <div className={styles.Experience4}>
+        <div className={styles.PhotoContent}>
+          <H1 className={styles.ExperienceHeading}>Grow together!</H1>
+          <H2>This year will be jam packed with awesome workshops.</H2>
         </div>
       </div>
     </div>
