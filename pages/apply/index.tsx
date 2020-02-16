4a2805a9d2d9558100b0389a43ec897fdab593b8
Amanda <amanda.haynes@versett.com>
2020-02-15 20:55:17 -0700

    apply page mobile styling

--- pages/apply/index.tsx
+++ pages/apply/index.tsx
@@ -3,20 +3,24 @@ import Head from "../../components/head";
 import { ReactTypeformEmbed } from "react-typeform-embed";
 import Layout from "../../components/Layout";
 
+import styles from "./index.module.scss";
+
 export default () => {
   return (
     <Layout>
       <Head title="Get Involved | Impact Music Festival" />
-      <ReactTypeformEmbed
-        style={{
-          "max-width": "900px",
-          "margin-left": "auto",
-          "margin-right": "auto",
-          left: 0,
-          right: 0
-        }}
-        url="https://dasrecord.typeform.com/to/elGZqC"
-      />
+      <div className={styles.Wrapper}>
+        <ReactTypeformEmbed
+          style={{
+            "max-width": "900px",
+            "margin-left": "auto",
+            "margin-right": "auto",
+            left: 0,
+            right: 0
+          }}
+          url="https://dasrecord.typeform.com/to/elGZqC"
+        />
+      </div>
     </Layout>
   );
 };
