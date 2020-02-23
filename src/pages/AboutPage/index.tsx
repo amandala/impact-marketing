faf3cb00157602635b2a439249922c923b5c9eb7
Amanda <amanda.haynes@versett.com>
2020-02-22 19:55:09 -0700

    footer padding large

--- src/pages/AboutPage/index.tsx
+++ src/pages/AboutPage/index.tsx
@@ -118,41 +118,6 @@ export default () => {
           </Section>
           <Section>
             <div className={styles.SectionHeading}>
-              <H1>Make impact</H1>
-            </div>
-            <P>
-              Out there somewhere in the deeps of space, on a world eerily
-              similar to our own, an intelligent species of biped revelled in
-              their prosperity, developing wonderous scientific advances and
-              economic prosperity. But... with the spoils of technology, came a
-              blissful ignorance towards the natural life and overall harmony on
-              the planet.
-            </P>
-            <P>
-              With the increase in toxic emissions slowly consuming the planet
-              this world soon reached a crisis point, causing a dramatic shift
-              in the climate and bringing about widespread devastation. Rising
-              seas soon claimed cities, natural disasters claimed entire
-              continents and their civilization fell apart. THE FEW WHO REMAINED
-              NEARLY LOST ALL HOPE.
-            </P>
-            <P>
-              The bipeds gathered their finest minds and launched a ship out
-              into space with the mission of sharing the STORY of their failed
-              world, preserving their culture and HOPING ANOTHER SPECIES WOULD
-              PAY HEED TO THE LESSON. Their story is a sad one... but.... we
-              have come to your planet because we have hope for ALL OF YOU, THE
-              IMPACT community.
-            </P>
-            <P>
-              Come and join us as we continue to tell this story through music,
-              art and various other means. Participate in our story with
-              interactive quests and multimedia displays.
-            </P>
-            <P>We Are the IMPACT...</P>
-          </Section>
-          <Section>
-            <div className={styles.SectionHeading}>
               <H1>Charities</H1>
             </div>
             <P>
@@ -199,6 +164,41 @@ export default () => {
               local, provincial, national, and international causes.
             </P>
             <H2>$50 from each ticket goes to one of our selected charities.</H2>
+          </Section>
+          <Section>
+            <div className={styles.SectionHeading}>
+              <H1>Make impact</H1>
+            </div>
+            <P>
+              Out there somewhere in the deeps of space, on a world eerily
+              similar to our own, an intelligent species of biped revelled in
+              their prosperity, developing wonderous scientific advances and
+              economic prosperity. But... with the spoils of technology, came a
+              blissful ignorance towards the natural life and overall harmony on
+              the planet.
+            </P>
+            <P>
+              With the increase in toxic emissions slowly consuming the planet
+              this world soon reached a crisis point, causing a dramatic shift
+              in the climate and bringing about widespread devastation. Rising
+              seas soon claimed cities, natural disasters claimed entire
+              continents and their civilization fell apart. THE FEW WHO REMAINED
+              NEARLY LOST ALL HOPE.
+            </P>
+            <P>
+              The bipeds gathered their finest minds and launched a ship out
+              into space with the mission of sharing the STORY of their failed
+              world, preserving their culture and HOPING ANOTHER SPECIES WOULD
+              PAY HEED TO THE LESSON. Their story is a sad one... but.... we
+              have come to your planet because we have hope for ALL OF YOU, THE
+              IMPACT community.
+            </P>
+            <P>
+              Come and join us as we continue to tell this story through music,
+              art and various other means. Participate in our story with
+              interactive quests and multimedia displays.
+            </P>
+            <P>We Are the IMPACT...</P>
           </Section>
         </div>
       </PhotoDiv>
