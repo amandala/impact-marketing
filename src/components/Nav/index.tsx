29e7e7f76e614a97bbfe3b4947cf843a3c6ed29f
Amanda <amanda.haynes@versett.com>
2020-02-23 17:55:35 -0700

    nav

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -18,37 +18,48 @@ export default () => {
 
   return (
     <div className={cx(styles.Wrapper, { [styles.WrapperFixed]: navOpen })}>
-      <button className={styles.Bun} onClick={() => setNavOpenState(!navOpen)}>
-        <div className={cx(styles.Burger, { [styles.BurgerAnimate]: navOpen })}>
-          <b>|</b>
-          <b>|</b>
-          <b>|</b>
-        </div>
-      </button>
-      <nav className={cx(styles.Nav, { [styles.NavFadeIn]: navOpen })}>
-        <div className={styles.Content}>
-          <ul className={styles.Links}>
-            <li onClick={() => setNavOpenState(false)}>
-              <Link href="/about">Home</Link>
-            </li>
-            <li onClick={() => setNavOpenState(false)}>
-              <Link href="/experience">Experience</Link>
-            </li>
-            <li onClick={() => setNavOpenState(false)}>
-              <Link href="/get-involved">Get Involved</Link>
-            </li>
-            <li onClick={() => setNavOpenState(false)}>
-              <Link href="/stages">Stages</Link>
-            </li>
-            {/* <li onClick={() => setNavOpenState(false)}>
+      <div>
+        <button
+          className={styles.Bun}
+          onClick={() => setNavOpenState(!navOpen)}
+        >
+          <div
+            className={cx(styles.Burger, { [styles.BurgerAnimate]: navOpen })}
+          >
+            <b>|</b>
+            <b>|</b>
+            <b>|</b>
+          </div>
+        </button>
+        <nav
+          className={cx(styles.Nav, {
+            [styles.NavFadeIn]: !!navOpen
+          })}
+        >
+          <div className={styles.Content}>
+            <ul className={styles.Links}>
+              <li onClick={() => setNavOpenState(false)}>
+                <Link href="/about">Home</Link>
+              </li>
+              <li onClick={() => setNavOpenState(false)}>
+                <Link href="/experience">Experience</Link>
+              </li>
+              <li onClick={() => setNavOpenState(false)}>
+                <Link href="/get-involved">Get Involved</Link>
+              </li>
+              <li onClick={() => setNavOpenState(false)}>
+                <Link href="/stages">Stages</Link>
+              </li>
+              {/* <li onClick={() => setNavOpenState(false)}>
               <Link href="/tickets">Tickets</Link>
             </li> */}
-            <li onClick={() => setNavOpenState(false)}>
-              <Link href="/lineup">Lineup</Link>
-            </li>
-          </ul>
-        </div>
-      </nav>
+              <li onClick={() => setNavOpenState(false)}>
+                <Link href="/lineup">Lineup</Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </div>
     </div>
   );
 };
