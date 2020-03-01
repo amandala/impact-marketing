c8ae591bde0d5005c1104bb664f135b14f6c131d
Amanda <amanda.haynes@versett.com>
2020-03-01 13:53:02 -0700

    reduce bg overlay opacity and  fix stages page logos

--- src/pages/Stages/index.tsx
+++ src/pages/Stages/index.tsx
@@ -18,36 +18,36 @@ export default () => {
             Each will be equipped with world class PK Sound and an
             out-of-this-world visual experience
           </P>
+          <div className={styles.MainLogos}>
+            <Link href="/odyssey">
+              <div className={styles.LogoWrapper}>
+                <img
+                  className={styles.Logo}
+                  src="/assets/odyssey.png"
+                  alt="Odyssey Stage"
+                />
+              </div>
+            </Link>
+            <Link href="/oasis">
+              <div className={styles.LogoWrapper}>
+                <img
+                  className={styles.Logo}
+                  src="/assets/oasis.png"
+                  alt="Oasis Stage"
+                />
+              </div>
+            </Link>
+            <Link href="/limitless">
+              <div className={styles.LogoWrapper}>
+                <img
+                  className={styles.Logo}
+                  src="/assets/limitless.png"
+                  alt="Limitless Stage"
+                />
+              </div>
+            </Link>
+          </div>
         </Section>
-        <div className={styles.MainLogos}>
-          <Link href="/odyssey">
-            <div className={styles.LogoWrapper}>
-              <img
-                className={styles.Logo}
-                src="/assets/odyssey.png"
-                alt="Odyssey Stage"
-              />
-            </div>
-          </Link>
-          <Link href="/oasis">
-            <div className={styles.LogoWrapper}>
-              <img
-                className={styles.Logo}
-                src="/assets/oasis.png"
-                alt="Oasis Stage"
-              />
-            </div>
-          </Link>
-          <Link href="/limitless">
-            <div className={styles.LogoWrapper}>
-              <img
-                className={styles.Logo}
-                src="/assets/limitless.png"
-                alt="Limitless Stage"
-              />
-            </div>
-          </Link>
-        </div>
       </PhotoDiv>
     </div>
   );
