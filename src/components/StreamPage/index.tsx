600555c2f32cccf7587fb26c06c9a55d17d12bf1
Amanda <amanda.haynes@versett.com>
2020-07-14 17:46:04 -0600

    join server instrucations

--- src/components/StreamPage/index.tsx
+++ src/components/StreamPage/index.tsx
@@ -2,9 +2,11 @@ import React from "react";
 import FestivalNav from "../../components/FestivalNav";
 import TwitchChannel from "../../components/TwitchChannel";
 import Layout from "../../components/Layout";
+import { H1, P } from "../../components/Typography";
 import WidgetBot from "@widgetbot/react-embed";
 import DonateButton from "../../components/DonateButton";
 import styles from "./index.module.scss";
+import { ButtonLinkExternal } from "../Button";
 
 export default ({
   channel,
@@ -37,8 +39,27 @@ export default ({
       </div>
       <DonateButton href="/causes" />
       <TwitchChannel channel={channel} />
-      <WidgetBot className={styles.Discord} server="713214250079748096" />
       <Layout black>
+        <div>
+          <H1 impact>Connect with the impact Tribe on Discord</H1>
+          <P>
+            Join in on the conversation. The server is embedded across all our
+            stages, so you can keep chatting with your friends as you explore.
+          </P>
+          <P>
+            Make sure you join the server before you start sending messages, or
+            they will disappear into the ether 👽
+          </P>
+          <div className={styles.JoinDiscord}>
+            <ButtonLinkExternal to="https://discord.gg/ACPxzJA">
+              Join the impact Discord server
+            </ButtonLinkExternal>
+          </div>
+        </div>
+        <WidgetBot className={styles.Discord} server="713214250079748096" />
+      </Layout>
+      <Layout black>
+        <H1 impact>Festival Navigation</H1>
         <FestivalNav />
       </Layout>
       {children}
