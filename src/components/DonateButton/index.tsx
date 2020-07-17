0f4a0a113335c88d4e3733d57352fa7155eae111
Amanda <amanda.haynes@versett.com>
2020-07-17 09:28:23 -0600

    Add artist fund donate button

--- src/components/DonateButton/index.tsx
+++ src/components/DonateButton/index.tsx
@@ -1,21 +1,31 @@
 import React from "react";
 import Section from "../../components/Section";
-import { ButtonLinkExternal, ButtonLink } from "../../components/Button";
-
+import { Tiny } from "../../components/Typography";
 import styles from "./index.module.scss";
 
 const DonateButton = ({ href }: { href: string }) => {
   return (
     <Section className={styles.DonateWrapper}>
-      {href === "/causes" ? (
-        <ButtonLink className={styles.Donate} to={href}>
-          Donate
-        </ButtonLink>
-      ) : (
-        <ButtonLinkExternal className={styles.Donate} to={href}>
-          Donate
-        </ButtonLinkExternal>
-      )}
+      <a className={styles.Donate} href={href}>
+        Donate
+        <img
+          className={styles.Logo}
+          src="/assets/stages/causes.png"
+          alt="Causes Logo"
+        />
+      </a>
+      <a
+        className={styles.Donate}
+        href="https://paypal.me/dusk2dawnproductions"
+      >
+        Donate
+        <img
+          className={styles.Logo}
+          src="/assets/causes/artist-fund-logo.png"
+          alt="Artist Fund Logo"
+        />
+        <Tiny>Impact Artist Fund</Tiny>
+      </a>
     </Section>
   );
 };
